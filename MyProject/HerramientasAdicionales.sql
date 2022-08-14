efca91f2c34cc868e0dd85e1433128ecbab567a2
Agustín Casado <agu.e.casado@gmail.com>
2022-08-14 12:03:02 -0300

    sp_help y sp_helptext

---
+++ MyProject/HerramientasAdicionales.sql
@@ -0,0 +1,13 @@
+-- Herramientas adicionales
+
+-- Comando SP_HELP
+-- Nos permite obtener detalles de objetos almacenados en la BD (tablas, stored procedures, vistas)
+
+SP_HELP Pais;
+SP_HELP Paciente;
+SP_HELP SP_ALTA_Turno;
+
+-- Comando SP_HELP_TEXT
+-- Nos permite obtener el contenido de un stored procedure
+
+SP_HELPTEXT SP_ALTA_Turno;
